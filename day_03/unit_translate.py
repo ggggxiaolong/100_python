8711979859baf1b2ddd37222c5ac6abdd8529f76
mrtan <freemrtan@gmail.com>
2018-09-26 20:40:54 +0800

    finish day03

---
+++ day_03/unit_translate.py
@@ -0,0 +1,18 @@
+"""
+
+英制单位英寸和公制单位厘米互换
+
+Version: 0.1
+Author: MrTan
+Date: 2018-09-24
+
+"""
+
+value = float(input('请输入长度:'))
+unit = input('请输入单位: ')
+if unit == 'in' or unit == '英寸':
+				print('%f英寸 = %f厘米' % (value, value * 2.54))
+elif unit == 'cm' or unit == '厘米':
+				print('%f厘米 = %f英寸' % (value, value / 2.54))
+else:
+				print('请输入有效的单位')
